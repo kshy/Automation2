304477f97bcc7e36db1e4ade311a252e87fa2c52
Tatyana Volovik <tvolovik@connecture.com>
2013-10-04 18:21:17 +0300

    Update with the latest changes

--- shpselenium/smoke_runtest_shopping.bat
+++ shpselenium/smoke_runtest_shopping.bat
@@ -8,13 +8,15 @@ Set currenttime=%Time::=-%
 Set currenttime=%currenttime:.=-%
 
 echo on
-::python -m robot.run -A chrome_args.txt --include shoppingANDconsumer --exclude draft --loglevel TRACE --outputdir "Reports\shopping\consumer" robot-tests\shopping
-::python -m robot.run -A chrome_args.txt --include shoppingANDagent --exclude draft --loglevel TRACE --outputdir "Reports\shopping\agent" robot-tests\shopping
+python -m robot.run -A chrome_args.txt --include shoppingANDconsumer --exclude draft --loglevel TRACE --outputdir "Reports\shopping\consumer" robot-tests\shopping
+python -m robot.run -A chrome_args.txt --include shoppingANDagent --exclude draft --loglevel TRACE --outputdir "Reports\shopping\agent" robot-tests\shopping
 ::python -m robot.run -A chrome_args.txt --test "Test Smoke test - Consumer completes shopping - Primary" --loglevel TRACE --outputdir "Reports\shopping" robot-tests\shopping
 ::python -m robot.run -A chrome_args.txt --test "Consumer completes shopping - Primary, Spouse, 2 Children" --loglevel TRACE --outputdir "Reports\shopping" robot-tests\shopping
-python -m robot.run -A chrome_args.txt --test "Consumer completes shopping - Child Only" --loglevel TRACE --outputdir "Reports\shopping" robot-tests\shopping
+::python -m robot.run -A chrome_args.txt --test "Consumer completes shopping - Child Only" --loglevel TRACE --outputdir "Reports\shopping" robot-tests\shopping
 ::python -m robot.run -A chrome_args.txt --test "Producer completes shopping - Primary" --loglevel TRACE --outputdir "Reports\shopping" robot-tests\shopping
-::python -m robot.run -A chrome_args.txt --test "Test Smoke test - Producer sends proposal and completes shopping - Primary" --loglevel TRACE --outputdir "Reports\shopping" robot-tests\shopping
+::python -m robot.run -A chrome_args.txt --test "Test Smoke test - Producer sends proposal and completes shopping - Primary" --loglevel TRACE --outputdir "Reports\shopping\1" robot-tests\shopping
 ::python -m robot.run -A chrome_args.txt --test "Producer completes shopping - Family of 4" --loglevel TRACE --outputdir "Reports\shopping" robot-tests\shopping
+::python -m robot.run -A chrome_args.txt --test "Producer sends proposal and completes shopping - Child Only" --loglevel TRACE --outputdir "Reports\shopping\2" robot-tests\shopping
+::python -m robot.run -A chrome_args.txt --test "Sales starts application and forward it to consumer - Primary" --loglevel TRACE --outputdir "Reports\shopping" robot-tests\shopping
 ::
 ::python -m robot.run -A local_args.txt src/test/resources/
